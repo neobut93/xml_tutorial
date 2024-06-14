f5aa5c311b68002ddb2780c097d8acc43de44f52
vadim <neobut93@gmail.com>
2024-06-14 13:50:44 -0700

    added toolbar

--- app/src/main/java/com/kodeco/xml/MainActivity.kt
+++ app/src/main/java/com/kodeco/xml/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.kodeco.xml
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setTitle("")
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_search -> Toast.makeText(this, "You clicked settings icon", Toast.LENGTH_SHORT).show()
+            R.id.nav_settings -> Toast.makeText(this, "You clicked search icon", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
