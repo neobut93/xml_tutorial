687d56fdea90df5067b01d93c2a87b65667653b4
vadim <neobut93@gmail.com>
2024-06-16 16:06:34 -0700

    first commit

---
+++ app/src/main/java/com/kodeco/xml/MainAdapter.kt
@@ -0,0 +1,8 @@
+package com.kodeco.xml
+
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter: RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+    inner class MainViewHolder(val itemBinding: RecyclerviewItemBinding)
+}
