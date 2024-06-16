d87b307ddd7c0415f111528d5043168ab00a44bd
vadim <neobut93@gmail.com>
2024-06-16 15:17:52 -0700

    added fragments

--- app/src/main/java/com/kodeco/xml/MainActivity.kt
+++ app/src/main/java/com/kodeco/xml/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.kodeco.xml
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragmentButton = findViewById<Button>(R.id.btnFragment1)
+        val secondFragmentButton = findViewById<Button>(R.id.btnFragment2)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        firstFragmentButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                //is used to add fragment in stack
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        secondFragmentButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                //is used to add fragment in stack
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
