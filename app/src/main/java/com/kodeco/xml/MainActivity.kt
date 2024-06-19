65bc0fc9b858a64311720102f60249d5f884d757
vadim <neobut93@gmail.com>
2024-06-19 15:58:57 -0700

    added service

--- app/src/main/java/com/kodeco/xml/MainActivity.kt
+++ app/src/main/java/com/kodeco/xml/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kodeco.xml
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,18 +14,30 @@ class MainActivity : AppCompatActivity() {
 
         val startButton = findViewById<Button>(R.id.btnStart)
         val stopButton = findViewById<Button>(R.id.btnStop)
-        val text = findViewById<TextView>(R.id.textView)
+        val sendButton = findViewById<Button>(R.id.btnSendData)
+        val serviceStatus = findViewById<TextView>(R.id.textView)
+        val editTextField = findViewById<EditText>(R.id.etField)
 
         startButton.setOnClickListener {
-            Intent(this, MyIntentService::class.java).also {
+            Intent(this, MyService::class.java).also {
                 startService(it)
-                text.text = "Service running"
+                serviceStatus.text = "Service running"
             }
         }
 
         stopButton.setOnClickListener {
-            MyIntentService.stopService()
-            text.text = "Service stopped"
+            Intent(this, MyService::class.java).also {
+                stopService(it)
+                serviceStatus.text = "Service stopped"
+            }
+        }
+
+        sendButton.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                val dataString = editTextField.text.toString()
+                it.putExtra("EXTRA_DATA", dataString)
+                startService(it)
+            }
         }
     }
 }
