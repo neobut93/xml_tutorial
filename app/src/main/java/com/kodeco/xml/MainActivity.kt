d64426a9243c3dbff71937c2d224bc3a7c66025d
vadim <neobut93@gmail.com>
2024-06-20 19:57:26 -0700

    added broadcast receiver

--- app/src/main/java/com/kodeco/xml/MainActivity.kt
+++ app/src/main/java/com/kodeco/xml/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.kodeco.xml
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: AirplaneModeChangedReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        receiver = AirplaneModeChangedReceiver()
+
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(receiver, it)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
     }
 }
