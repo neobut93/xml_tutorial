bb8bdc629271453db1c90947f1933da9ea92b47a
vadim <neobut93@gmail.com>
2024-06-20 19:30:05 -0700

    added drag and drop layout

--- app/src/main/java/com/kodeco/xml/MainActivity.kt
+++ app/src/main/java/com/kodeco/xml/MainActivity.kt
@@ -1,11 +1,80 @@
 package com.kodeco.xml
 
+import android.content.ClipData
+import android.content.ClipDescription
 import android.os.Bundle
+import android.view.DragEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dragView = findViewById<View>(R.id.dragView)
+        val top = findViewById<LinearLayout>(R.id.llTop)
+        val bottom = findViewById<LinearLayout>(R.id.llBottom)
+
+        top.setOnDragListener(dragListener)
+        bottom.setOnDragListener(dragListener)
+
+        dragView.setOnLongClickListener {
+            val clipText = "This is our ClipData text"
+            val item = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item)
+
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+        }
+    }
+
+    val dragListener = View.OnDragListener { view, event ->
+        when (event.action) {
+            DragEvent.ACTION_DRAG_STARTED -> {
+                event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            }
+
+            DragEvent.ACTION_DRAG_ENTERED -> {
+                view.invalidate()
+                true
+            }
+
+            DragEvent.ACTION_DRAG_LOCATION -> true
+            DragEvent.ACTION_DRAG_EXITED -> {
+                view.invalidate()
+                true
+            }
+
+            DragEvent.ACTION_DROP -> {
+                val item = event.clipData.getItemAt(0)
+                val dragData = item.text
+                Toast.makeText(this, dragData, Toast.LENGTH_SHORT).show()
+
+                view.invalidate()
+
+                val v = event.localState as View
+                val owner = v.parent as ViewGroup
+                owner.removeView(v)
+                val destination = view as LinearLayout
+                destination.addView(v)
+                v.visibility = View.VISIBLE
+                true
+            }
+
+            DragEvent.ACTION_DRAG_ENDED -> {
+                view.invalidate()
+                true
+            }
+
+            else -> false
+        }
     }
 }
